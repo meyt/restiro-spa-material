70515decebb550e03664fa31a71bd1641c36b6fa
meyt <pasd3000@gmail.com>
2019-04-22 12:16:27 +0430

    Fix code styles

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import './assets/main.styl'
 
 Vue.config.productionTip = false
 
